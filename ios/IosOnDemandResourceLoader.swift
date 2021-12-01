bccde17d787bd7c0ac00c6a865cadd02b15ef04b
slava <vyacheslav.troyan@yellowsocialinteractive.com>
2021-12-01 23:45:07 +0200

    improve loadResourcesByTag swift module

--- ios/IosOnDemandResourceLoader.swift
+++ ios/IosOnDemandResourceLoader.swift
@@ -1,37 +1,44 @@
 import Foundation
 
+
 @objc(IosOnDemandResourceLoader)
 class IosOnDemandResourceLoader: NSObject {
     var request: NSBundleResourceRequest?
 
-
-
     @objc(loadResourcesByTag:resolver:rejecter:)
      func loadResourcesByTag(tag: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-         guard self.request == nil else {return}
+        self.request?.endAccessingResources()
 
          self.request = NSBundleResourceRequest(tags: [tag])
 
          self.request?.conditionallyBeginAccessingResources(completionHandler: { (resourceAvailable) in
-             if !resourceAvailable { //resource not available locally
+           DispatchQueue.main.async {
+             if !resourceAvailable { // resource not available locally
+               print("resource not available locally")
                  self.request!.beginAccessingResources { err in
                      guard err == nil else {
-                         //an error occured while downloading
-                         reject("err_while_downloading", "an error occured while downloading", err)
-                         return
-                     }
-                     resolve("file_downloaded")
-                     return
+                        // an error occured while downloading
+                        print("an error occured while downloading")
+                        reject("err_while_downloading", "an error occured while downloading", err)
+                        return
+                    }
+
+                    let path = Bundle.main.url(forResource: tag, withExtension: "zip")
+
+                    self.request?.endAccessingResources()
+                    resolve(path?.absoluteString)
+                    return
                  }
-             } else { //resource available locally
-                 resolve("available_locally")
+             } else { // resource available locally
+               print("resource available locally")
+               let path = Bundle.main.url(forResource: tag, withExtension: "zip")
+
+               self.request?.endAccessingResources()
+               resolve(path?.absoluteString)
              }
+
+           }
          })
-
-    }
-
-    func cancelRequest() {
-      self.request?.progress.cancel()
     }
 
 }
